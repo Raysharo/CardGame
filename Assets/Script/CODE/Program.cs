06582a57d68ccfeee7ea08903f156d470ed6c1dd
unknown <lucaslyonbusiness@gmail.com>
2023-12-04 14:15:43 +0100

    add socket connexion

--- Assets/Script/CODE/Program.cs
+++ Assets/Script/CODE/Program.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 
 public class Program : MonoBehaviour
 {
-   
+    private WebSocket webSocket;
+
     void Start()
     {
+        SocketConnexion();
        createGoldCard();
 
     }
@@ -31,6 +33,19 @@ public class Program : MonoBehaviour
         GameObject monsterCard = new GameObject("MonsterCard");
         monsterCard.AddComponent<MonsterCard>();
         // monsterCard.AddComponent<Card>();
+    }
+
+    void SocketConnexion(){
+        //webSocket = new WebSocket("wss://localhost:3000/0");
+        string adresseNgrok = AdresseSingleton.GetSharedValue();
+        webSocket = new WebSocket("wss://" + adresseNgrok + ".ngrok-free.app/0");
+        webSocket.OnMessage += (sender, e) =>
+        {
+            Debug.Log("Message reçu du serveur : " + e.Data);
+            // Ajoutez ici la logique pour traiter les messages reçus du serveur (éventuellement des messages des joueurs)
+        };
+
+        webSocket.Connect();
     }
 
 }
