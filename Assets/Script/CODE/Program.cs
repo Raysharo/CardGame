76c43ab21652c1d21beea6d023ba635109ee64ee
unknown <lucaslyonbusiness@gmail.com>
2023-12-04 14:56:22 +0100

    server + client socket

--- Assets/Script/CODE/Program.cs
+++ Assets/Script/CODE/Program.cs
@@ -2,17 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using System;
+using System.Diagnostics;
+
+using WebSocketSharp;
+
 
 public class Program : MonoBehaviour
 {
-    private WebSocket webSocket;
+        private WebSocket webSocket;
+
 
     void Start()
     {
-        SocketConnexion();
-       createGoldCard();
+        // SocketConnexion();
+        createGoldCard();
+
+
+
+
+        //webSocket = new WebSocket("wss://localhost:3000/0");
+        // string adresseNgrok = AdresseSingleton.GetSharedValue();
+        string adresseNgrok = "e740-46-193-3-79";
+        webSocket = new WebSocket("wss://" + adresseNgrok + ".ngrok-free.app/0");
+        webSocket.OnMessage += (sender, e) =>
+        {
+            UnityEngine.Debug.Log("Message reçu du serveur : " + e.Data);
+            // Ajoutez ici la logique pour traiter les messages reçus du serveur (éventuellement des messages des joueurs)
+        };
+
+        webSocket.Connect();
+
+        // Envoyer un message aux joueurs
+        SendMessageToPlayers("Message de la Lucas!");
+
 
     }
+
+     // Fonction pour envoyer un message aux joueurs depuis la table
+    public void SendMessageToPlayers(string message)
+    {
+        if (webSocket != null && webSocket.ReadyState == WebSocketState.Open)
+        {
+            webSocket.Send(message);
+        }
+    }   
 
     void createGoldCard()
     {
@@ -36,16 +70,32 @@ public class Program : MonoBehaviour
     }
 
     void SocketConnexion(){
-        //webSocket = new WebSocket("wss://localhost:3000/0");
-        string adresseNgrok = AdresseSingleton.GetSharedValue();
-        webSocket = new WebSocket("wss://" + adresseNgrok + ".ngrok-free.app/0");
-        webSocket.OnMessage += (sender, e) =>
-        {
-            Debug.Log("Message reçu du serveur : " + e.Data);
-            // Ajoutez ici la logique pour traiter les messages reçus du serveur (éventuellement des messages des joueurs)
-        };
+        // Spécifiez le chemin du fichier script.js
+        string cheminDuScript = "Server/server.js";
 
-        webSocket.Connect();
+        // Créez un processus pour exécuter Node.js
+        Process processNode = new Process();
+        processNode.StartInfo.FileName = "node"; // Assurez-vous que "node" est dans le PATH
+        processNode.StartInfo.Arguments = cheminDuScript;
+        processNode.StartInfo.RedirectStandardOutput = true;
+        processNode.StartInfo.UseShellExecute = false;
+        processNode.StartInfo.CreateNoWindow = true;
+
+        // Démarrez le processus
+        processNode.Start();    
+
+        // // Obtenez la sortie standard (résultats de l'exécution du script)
+        // string resultat = processNode.StandardOutput.ReadToEnd();
+
+        // // Attendez la fin de l'exécution du processus
+        // processNode.WaitForExit();
+
+        // // Affichez la sortie du script Node.js
+        // Console.WriteLine("Résultat de l'exécution du script Node.js :");
+        // Console.WriteLine(resultat);
+
+        // // Fermez le processus
+        // processNode.Close();
     }
 
 }
