e68a9f2d9f67b93505f6c5888c5be61e0e7ab49a
ELYSALEM99 <elysalemm5@gmail.com>
2024-01-13 03:24:59 +0100

    modif tableScene

--- Assets/Script/CODE/NgrokManager.cs
+++ Assets/Script/CODE/NgrokManager.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class NgrokManager : MonoBehaviour
 {
-    private static string adresseNgrok = "e6d6-134-59-215-253";
+    private static string adresseNgrok = "912a-46-193-3-79";
 
 
     public static string GetAdresseNgrok()
