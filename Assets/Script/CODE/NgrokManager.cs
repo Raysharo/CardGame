381399f86d6f5a705aaa5f7c574363df8f09b8cc
Raysharo <lucaslyonbusiness@gmail.com>
2024-01-15 11:13:56 +0100

    start implement physical coin

--- Assets/Script/CODE/NgrokManager.cs
+++ Assets/Script/CODE/NgrokManager.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class NgrokManager : MonoBehaviour
 {
-    private static string adresseNgrok = "7161-46-193-3-79";
+    private static string adresseNgrok = "13e2-134-59-191-33";
 
 
     public static string GetAdresseNgrok()
